d85c5b3cfd55fca214f71667acc6e8d5c391ef6e
Amey Sunu <ameysunu00719@gmail.com>
2021-01-23 00:47:50 +0300

    google sign in added

---
+++ lib/homewidget.dart
@@ -0,0 +1,68 @@
+import 'package:badgerhacks/home.dart';
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+class HomeWidget extends StatefulWidget {
+  @override
+  _HomeWidgetState createState() => _HomeWidgetState();
+}
+
+class _HomeWidgetState extends State<HomeWidget> {
+  @override
+  int _selectedIndex = 0;
+
+  final List<Widget> _widgetOptions = [
+    Home(),
+  ];
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: HexColor('#FFC1B2'),
+          type: BottomNavigationBarType.fixed,
+          currentIndex: _selectedIndex,
+          showSelectedLabels: true,
+          items: [
+            BottomNavigationBarItem(
+              icon: new Icon(Icons.library_books, color: HexColor('#545E78')),
+              title: new Text(
+                'Tutorials',
+                style: TextStyle(
+                    color: HexColor('#545E78'), fontFamily: 'Roboto Medium'),
+              ),
+            ),
+            BottomNavigationBarItem(
+              icon:
+                  new Icon(Icons.horizontal_split, color: HexColor('#545E78')),
+              title: new Text(
+                'Split',
+                style: TextStyle(
+                    color: HexColor('#545E78'), fontFamily: 'Roboto Medium'),
+              ),
+            ),
+            BottomNavigationBarItem(
+              icon: new Icon(Icons.help_center, color: HexColor('#545E78')),
+              title: new Text(
+                'Quiz',
+                style: TextStyle(
+                    color: HexColor('#545E78'), fontFamily: 'Roboto Medium'),
+              ),
+            ),
+            BottomNavigationBarItem(
+              icon: new Icon(Icons.person, color: HexColor('#545E78')),
+              title: new Text(
+                'User',
+                style: TextStyle(
+                    color: HexColor('#545E78'), fontFamily: 'Roboto Medium'),
+              ),
+            ),
+          ],
+          onTap: (index) {
+            setState(() {
+              _selectedIndex = index;
+            });
+          }),
+      body: _widgetOptions.elementAt(_selectedIndex),
+    );
+  }
+}
--- lib/login.dart
+++ lib/login.dart
@@ -1,5 +1,50 @@
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+import 'homewidget.dart';
+
+final FirebaseAuth _auth = FirebaseAuth.instance;
+final GoogleSignIn googleSignIn = GoogleSignIn();
+
+Future<String> signInWithGoogle() async {
+  await Firebase.initializeApp();
+
+  final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
+  final GoogleSignInAuthentication googleSignInAuthentication =
+      await googleSignInAccount.authentication;
+
+  final AuthCredential credential = GoogleAuthProvider.credential(
+    accessToken: googleSignInAuthentication.accessToken,
+    idToken: googleSignInAuthentication.idToken,
+  );
+
+  final UserCredential authResult =
+      await _auth.signInWithCredential(credential);
+  final User user = authResult.user;
+
+  if (user != null) {
+    assert(!user.isAnonymous);
+    assert(await user.getIdToken() != null);
+
+    final User currentUser = _auth.currentUser;
+    assert(user.uid == currentUser.uid);
+
+    print('signInWithGoogle succeeded: $user');
+
+    return '$user';
+  }
+
+  return null;
+}
+
+Future<void> signOutGoogle() async {
+  await googleSignIn.signOut();
+
+  print("User Signed Out");
+}
 
 class Login extends StatefulWidget {
   @override
@@ -77,17 +122,17 @@ class _LoginState extends State<Login> {
                   borderRadius: BorderRadius.circular(10.0),
                 ),
                 onPressed: () {
-                  // signInWithGoogle().then((result) {
-                  //   if (result != null) {
-                  //     Navigator.of(context).push(
-                  //       MaterialPageRoute(
-                  //         builder: (context) {
-                  //           return HomeWidget();
-                  //         },
-                  //       ),
-                  //     );
-                  //   }
-                  // });
+                  signInWithGoogle().then((result) {
+                    if (result != null) {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return HomeWidget();
+                          },
+                        ),
+                      );
+                    }
+                  });
                 },
               ),
             ),

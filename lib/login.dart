56f6e479b9872a0b8da939b915158438574047a4
Amey Sunu <ameysunu00719@gmail.com>
2021-01-23 00:09:19 +0300

    login init

---
+++ lib/login.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: HexColor('#FFE8F7'),
+    );
+  }
+}

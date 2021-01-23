4ba6be75736dc96e5ff9458d830788b3423abe32
Amey Sunu <ameysunu00719@gmail.com>
2021-01-23 21:33:01 +0300

    ml vision done

--- lib/flyers.dart
+++ lib/flyers.dart
@@ -1,11 +1,15 @@
 import 'dart:io';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:hexcolor/hexcolor.dart';
-
 import 'package:image_picker/image_picker.dart';
 import 'package:firebase_ml_vision/firebase_ml_vision.dart';
+import 'pages/addnote.dart';
 
+AsyncSnapshot<DocumentSnapshot> snapshot;
+Stream<QuerySnapshot> newStream;
 void main() => runApp(Flyer());
 
 class Flyer extends StatelessWidget {
@@ -57,6 +61,19 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        actions: [
+          IconButton(
+              icon: Icon(Icons.view_agenda, color: Colors.black),
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return MyFlyers();
+                    },
+                  ),
+                );
+              })
+        ],
         elevation: 0,
         backgroundColor: Colors.white,
         automaticallyImplyLeading: false,
@@ -91,16 +108,6 @@ class _MyHomePageState extends State<MyHomePage> {
                 : Container(),
             SizedBox(height: 10.0),
             Center(
-              // child: InkWell(
-              //   child: SizedBox(
-              //     height: 200,
-              //     width: 200,
-              //     child: Image.asset('images/create.png'),
-              //   ),
-              //   onTap: () async {
-              //     pickImage();
-              //   },
-              // ),
               child: Padding(
                 padding: const EdgeInsets.all(10.0),
                 child: ButtonTheme(
@@ -140,7 +147,9 @@ class _MyHomePageState extends State<MyHomePage> {
                         padding: const EdgeInsets.all(10.0),
                         child: SelectableText(
                           text,
-                          style: TextStyle(fontFamily: 'Roboto'),
+                          style: TextStyle(
+                              fontFamily: 'ArimaMadurai',
+                              fontWeight: FontWeight.bold),
                         ),
                       ),
                     ),
@@ -153,8 +162,18 @@ class _MyHomePageState extends State<MyHomePage> {
                   shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(20.0),
                       side: BorderSide(color: HexColor('#FFFFFF'))),
-                  onPressed: () {
-                    null;
+                  onPressed: () async {
+                    firestoreInstance
+                        .collection('flyers')
+                        .doc(
+                            '${now.hour.toString()}${now.minute.toString()}${now.second.toString()}')
+                        .set({
+                      "flyer": text,
+                      "id":
+                          '${now.hour.toString()}${now.minute.toString()}${now.second.toString()}',
+                    }).then((_) {
+                      print("success!");
+                    });
                   },
                   color: Colors.white,
                   textColor: HexColor('#000000'),
@@ -171,6 +190,138 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class MyFlyers extends StatefulWidget {
+  @override
+  _MyFlyersState createState() => _MyFlyersState();
+}
+
+class _MyFlyersState extends State<MyFlyers> {
+  @override
+  void initState() {
+    super.initState();
+    newStream = firestoreInstance.collection('flyers').snapshots();
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+              icon: Icon(Icons.cancel, color: Colors.black),
+              onPressed: () {
+                Navigator.pop(context);
+              })
+        ],
+        elevation: 0,
+        backgroundColor: Colors.white,
+        automaticallyImplyLeading: false,
+        title: Text('Flyers'.toUpperCase(),
+            style: TextStyle(
+              fontFamily: 'ArimaMadurai',
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            )),
+      ),
+      backgroundColor: Colors.white,
+      body: Container(
+        child: Container(
+          child: StreamBuilder(
+            stream: newStream,
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              var totalgroupCount = 0;
+              List<DocumentSnapshot> groupUsers;
+              if (snapshot.hasData) {
+                groupUsers = snapshot.data.documents;
+                totalgroupCount = groupUsers.length;
+                return (Container(
+                  child: ListView.builder(
+                      itemCount: groupUsers.length,
+                      itemBuilder: (context, int index) {
+                        return SingleChildScrollView(
+                          child: Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: Slidable(
+                              actionPane: SlidableDrawerActionPane(),
+                              actionExtentRatio: 0.25,
+                              child: Container(
+                                child: Column(
+                                  children: [
+                                    Container(
+                                        decoration: BoxDecoration(
+                                          border:
+                                              Border.all(color: Colors.black),
+                                          borderRadius:
+                                              BorderRadius.circular(15.0),
+                                        ),
+                                        width:
+                                            MediaQuery.of(context).size.width *
+                                                1,
+                                        child: Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            Padding(
+                                              padding:
+                                                  const EdgeInsets.all(10.0),
+                                              child: Text(
+                                                  groupUsers[index]
+                                                      .data()['flyer'],
+                                                  style: TextStyle(
+                                                      fontFamily:
+                                                          'ArimaMadurai',
+                                                      fontSize: 20,
+                                                      color:
+                                                          HexColor('#000000'))),
+                                            ),
+                                          ],
+                                        )),
+                                  ],
+                                ),
+                              ),
+                              secondaryActions: <Widget>[
+                                Padding(
+                                  padding: const EdgeInsets.all(10.0),
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(15.0),
+                                      color: HexColor('#FF0D0D'),
+                                    ),
+                                    child: Container(
+                                      child: IconButton(
+                                        icon: Icon(Icons.delete,
+                                            color: Colors.white),
+                                        onPressed: () {
+                                          print("Delete triggered");
+                                          firestoreInstance
+                                              .collection('flyers')
+                                              .doc(groupUsers[index]
+                                                  .data()['id'])
+                                              .delete()
+                                              .then((_) {
+                                            print("success!");
+                                          });
+                                        },
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      }),
+                ));
+              } else {
+                return Center(child: CircularProgressIndicator());
+              }
+            },
+          ),
         ),
       ),
     );
